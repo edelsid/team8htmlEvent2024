e6c15dd74b064803fcb2220e56445116f721f9b3
Ilya Burch <ilyaburch@yandex.ru>
2024-09-29 13:27:52 +0300

    add animation to header nav

--- src/header/header.js
+++ src/header/header.js
@@ -3,7 +3,7 @@ const observer = new window.IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       document.querySelectorAll('.header__black__bottom__navitem').forEach((link) => {
-        let id = link.getAttribute('href').replace('#', '')
+        const id = link.getAttribute('href').replace('#', '')
         if (id === entry.target.id) {
           link.classList.add('active')
         } else {
