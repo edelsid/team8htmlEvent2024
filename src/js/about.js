04847840792d6d8cd79822c2dd49f39db59923d9
edelsid <eradonna@yandex.ru>
2024-12-25 03:25:13 +0300

    about slider animation completed

--- src/js/about.js
+++ src/js/about.js
@@ -7,49 +7,85 @@ export default class About {
       id: 1,
       url: "/assets/img/about1.webp",
       title: "Our vision",
+      characteristics: [
+        "Integrity and ethics", 
+        "Commitment to quality", 
+        "Innovation and creativity"
+      ],
     }, 
     {
       id: 2,
       url: "/assets/img/about2.webp",
       title: "Core Values",
+      characteristics: [
+        "Responsibility", 
+        "Communication", 
+        "Attention to detail"
+      ],
     }, 
     {
       id: 3,
       url: "/assets/img/about3.webp",
       title: "Community Engagement",
+      characteristics: [
+        "Personal contact partners",
+        "Community events", 
+        "24 hour support",
+        "Conference participation"
+      ],
     }, 
     {
       id: 4,
       url: "/assets/img/about4.webp",
       title: "Our Reach",
+      characteristics: [
+        "Offices in 10 countries", 
+        "Partners across the globe", 
+      ],
     }, 
     {
       id: 5,
       url: "/assets/img/about5.webp",
       title: "Commitment to Quality",
+      characteristics: [
+        "Only the best materials", 
+        "Experienced workers",
+        "Cultivated reputation",
+      ],
     }];
-    this.activeSliders = [];
+    this.activeSliders = container.querySelectorAll(".about__item");
+    this.contents = [];
     this.active = 1;
   }
 
   bindToDOM(container) {
     checkDOM(container);
-    this.sliderArea = container.querySelector(".about__sliders");
     this.arrows = container.querySelectorAll(".arrow");
   }
 
   init() {
-    this.bindSliders();
     this.arrows.forEach((arrow) => {
       arrow.addEventListener("click", (e) => this.changeSlide(e.currentTarget));
     });
-    this.activeSliders = this.sliderArea.getElementsByTagName('li');
+    for (let i = 0; i < this.activeSliders.length; i += 1) {
+      this.contents.push(this.sliders[i]);
+    };
+    this.bindSliders();
   }
 
   bindSliders() {
-    for (let i = 0; i < 3; i += 1) {
-      const starterSlider = this.sliderFormation(this.sliders[i]);
-      this.sliderArea.appendChild(starterSlider);
+    for (let i = 0; i < this.activeSliders.length; i += 1) {
+      const starterSlider = this.sliderFormation(this.contents[i]);
+      this.activeSliders[i].style.backgroundImage = `url(${this.contents[i].url})`;
+      this.activeSliders[i].innerHTML = "";
+      if (this.contents[i] === this.sliders[this.active]) {
+        const characteristics = this.listFormation(this.contents[i]);
+        starterSlider.appendChild(characteristics);
+        setTimeout(() => {
+          starterSlider.style.opacity = 1;
+        }, 300);
+      };
+      this.activeSliders[i].appendChild(starterSlider);
     };
   }
 
@@ -67,10 +103,10 @@ export default class About {
     if (this.active > this.sliders.length - 1) {
       this.active = 0;
     };
-    const newSlider = this.sliderFormation(this.sliders[this.active + 1] ||
+    this.contents.shift();
+    this.contents.push(this.sliders[this.active + 1] ||
       this.sliders[0]);
-    this.sliderArea.removeChild(this.activeSliders[0]);
-    this.sliderArea.appendChild(newSlider);
+    this.bindSliders();
   }
 
   back() {
@@ -78,38 +114,28 @@ export default class About {
     if (this.active < 0) {
       this.active = this.sliders.length - 1;
     };
-    const newSlider = this.sliderFormation(this.sliders[this.active - 1] ||
+    this.contents.pop();
+    this.contents.unshift(this.sliders[this.active - 1] ||
       this.sliders[this.sliders.length - 1]);
-    this.sliderArea.removeChild(this.activeSliders[this.activeSliders.length - 1]);
-    this.sliderArea.insertBefore(newSlider, this.sliderArea.firstElementChild);
+    this.bindSliders();
   }
 
   sliderFormation(obj) {
-    const newSlider = document.createElement("li");
-    newSlider.classList = "about__item";
-    newSlider.id = `about-${obj.id}`;
-    newSlider.style.backgroundImage = `url(${obj.url})`;
-    newSlider.innerHTML = `
-    <div class="slider__content">
-      <h3 class="title title-accent">${obj.title}</h3>
-    </div>
-    `;
+    const newSlider = document.createElement("div");
+    newSlider.classList = "slider__content";
+    newSlider.innerHTML = `<h3 class="title title-accent">${obj.title}</h3>`;
     return newSlider;
   }
 
-  listFormation() {
+  listFormation(content) {
     const newMsg = document.createElement("ul");
     newMsg.className = "slider__list";
-    newMsg.innerHTML = `
-    <li class="subtitle subtitle-list">
-      Integrity and ethics
-    </li>
-    <li class="subtitle subtitle-list">
-      Commitment to quality
-    </li>
-    <li class="subtitle subtitle-list">
-      Innovation and creativity
-    </li>`;
+    for (let i = 0; i < content.characteristics.length; i += 1) {
+      const listItem = document.createElement("li");
+      listItem.className = "subtitle subtitle-list";
+      listItem.innerText = content.characteristics[i];
+      newMsg.appendChild(listItem);
+    };
     return newMsg;
   }
 }
